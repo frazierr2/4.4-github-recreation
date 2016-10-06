26d6aa46bc953739b167bb04ec1f107163f69265
Ryan Frazier <frazierr2@gmail.com>
2016-10-06 16:58:59 -0400

    Header created

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var Handlebars = require('handlebars');
+var githubtoken = require('./gitapikey.js');
+
+if(githubtoken !== undefined){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + githubtoken.token
+    }
+  });
+}
