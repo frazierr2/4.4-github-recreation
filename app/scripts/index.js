b6eeb3fda7f150f6459bcf45ce1f20335ee79de1
Ryan Frazier <frazierr2@gmail.com>
2016-10-06 22:51:57 -0400

    Updates to template

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -10,3 +10,23 @@ if(githubtoken !== undefined){
     }
   });
 }
+
+var url = 'https://api.github.com';
+var userInfo = url + '/users/frazierr2';
+console.log(userInfo);
+
+
+$.ajax(userInfo).then(userThumnail);
+
+function userThumnail(data){
+  console.log(data.avatar_url);
+  // var planets = data.results;
+  // var $planetContainer = $('#planet-list');
+  //
+  // var source = $('#plaent-template').html();
+  // var template = Handlebars.compile(source);
+  //
+  // _.each(planets, function(planet){
+  //   $planetContainer.append(template(planet));
+  // });
+}
